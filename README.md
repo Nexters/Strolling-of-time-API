3c5d16b4e6643936527f196b2cf07b27c3a83e76
Mincheol Kim <mcauto@naver.com>
2019-07-22 06:57:17 +0900

    init project, swagger, test, layered architecture(DDD)

---
+++ README.md
@@ -0,0 +1,19 @@
+# 시간의 산책
+
+Domain driven design layered architecture (UI - application - domain - infra)
+
+https://archfirst.org/domain-driven-design-6-layered-architecture/
+
+## 환경 구성
+
+```
+$ docker-compose up -d
+```
+
+
+## Healthcheck path
+
+spring-actuator
+```
+http://{domain}/actuator/health
+```
---
+++ src/main/java/com/nexters/wiw/api/service/UserService.java
@@ -0,0 +1,25 @@
+package com.nexters.wiw.api.service;
+
+import com.nexters.wiw.api.domain.User;
+import com.nexters.wiw.api.domain.UserRepository;
+import com.nexters.wiw.api.ui.UserRequestDto;
+import com.nexters.wiw.api.ui.UserResponseDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+@Service
+@Transactional(readOnly = true)
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Transactional
+    public UserResponseDto save(UserRequestDto userRequestDto) {
+        User savedUser = userRepository.save(userRequestDto.of());
+        return new UserResponseDto();
+    }
+
+}
