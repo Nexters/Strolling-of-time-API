0eaf453b2b4c9f85cafa9a827da132b520e8fd9b
Mincheol Kim <mcauto@naver.com>
2019-08-17 14:58:52 +0900

    ㅠㅠ 제거

--- src/main/java/com/nexters/wiw/api/service/UserService.java
+++ src/main/java/com/nexters/wiw/api/service/UserService.java
@@ -9,8 +9,6 @@ import com.nexters.wiw.api.exception.UserNotExistedException;
 import com.nexters.wiw.api.ui.UserRequestDto;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,16 +16,12 @@ import org.springframework.transaction.annotation.Transactional;
 import lombok.extern.slf4j.Slf4j;
 
 @Service
-@Slf4j
 public class UserService {
     @Autowired
     private UserRepository userRepository;
 
     @Autowired
     private PasswordEncoder bCryptPasswordEncoder;
-
-    @Autowired
-    private AuthenticationManager authenticationManager;
 
     public User getOne(Long id) {
         return userRepository.findById(id)
