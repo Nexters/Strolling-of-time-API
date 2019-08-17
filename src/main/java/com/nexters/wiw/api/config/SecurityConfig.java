fda55584488f51a8284647ea075545e5105af678
박다예 <sju02030@gmail.com>
2019-08-17 14:57:00 +0900

    securitconfig 파일 수정

--- src/main/java/com/nexters/wiw/api/config/SecurityConfig.java
+++ src/main/java/com/nexters/wiw/api/config/SecurityConfig.java
@@ -1,8 +1,5 @@
 package com.nexters.wiw.api.config;
 
-import com.nexters.wiw.api.common.JwtAuthenticationEntryPoint;
-import com.nexters.wiw.api.common.JwtRequestFilter;
-import com.nexters.wiw.api.domain.UserRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -14,15 +11,9 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Autowired
-    private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
-
-    @Autowired
-    private JwtRequestFilter JwtRequestFilter;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -39,15 +30,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 "/swagger-ui.html**",
                 "/webjars/**",
                 "favicon.ico").permitAll()
-            .anyRequest().authenticated()
-            .and()
-            // entrypoint 설정
-            .exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint)
-            .and()
-            // session 설정
-            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+            .anyRequest().authenticated();
 
-        // http.addFilterBefore(JwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
     }
 
     @Bean
@@ -55,9 +39,4 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
-    }
 }
