04f55622d1c045b1b3fddb8c6ed45a3905485754
Mincheol Kim <mcauto@naver.com>
2019-08-18 21:33:29 +0900

    프로세스 재시작 및 심볼릭 링크 변경을 위한 스크립트 추가

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+REPOSITORY=/home/ec2-user/app/travis
+
+APP_JAR=$(basename ${REPOSITORY}/build/*jar)
+
+DEPLOY_PATH=/var/www/wiw.nexters.com
+
+systemctl status wiw
+
+sudo cp $REPOSITORY/build/*.jar $DEPLOY_PATH/
+
+sudo ln -Tfs $DEPLOY_PATH/releases/$APP_JAR $DEPLOY_PATH/current
+
+sudo systemctl restart wiw
+
+sudo systemctl status wiw
