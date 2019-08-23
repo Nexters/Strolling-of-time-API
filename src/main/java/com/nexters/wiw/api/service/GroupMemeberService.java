d0093d72a53b375e9c1b6ffe4db1a865678a91af
nnm <hyeon930@gmail.com>
2019-08-23 22:38:26 +0900

    컨플릭트 해소2

--- src/main/java/com/nexters/wiw/api/service/GroupMemeberService.java
+++ src/main/java/com/nexters/wiw/api/service/GroupMemeberService.java
@@ -3,6 +3,7 @@ package com.nexters.wiw.api.service;
 import com.nexters.wiw.api.domain.*;
 import com.nexters.wiw.api.domain.error.ErrorType;
 import com.nexters.wiw.api.exception.GroupMemberNotFoundException;
+import com.nexters.wiw.api.exception.NotFoundException;
 import com.nexters.wiw.api.exception.UnAuthorizedException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -28,7 +29,7 @@ public class GroupMemeberService {
         if (!authService.isValidateToken(authHeader))
             throw new UnAuthorizedException(ErrorType.UNAUTHORIZED, "UNAUTHORIZED");
 
-        User user = userRepository.getOne(userId);
+        User user = userRepository.findById(userId).orElseThrow(() -> new NotFoundException(ErrorType.NOT_FOUND, "아이디에 해당하는 유저가 존재하지 않습니다."));
         Group group = groupRepository.getOne(groupId);
 
         GroupMember groupMember = new GroupMember(group, user, false);
@@ -41,7 +42,7 @@ public class GroupMemeberService {
         if (!authService.isValidateToken(authHeader))
             throw new UnAuthorizedException(ErrorType.UNAUTHORIZED, "UNAUTHORIZED");
 
-        User user = userRepository.getOne(userId);
+        User user = userRepository.findById(userId).orElseThrow(() -> new NotFoundException(ErrorType.NOT_FOUND, "아이디에 해당하는 유저가 존재하지 않습니다."));
         Group group = groupRepository.getOne(groupId);
 
         GroupMember groupMember = groupMemberRepository.findByGroupAndUser(group, user)
